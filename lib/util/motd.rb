456187b6973e0d4e1e8a0bc76d2277e90ccfe4e3
Avellea <dynamicdonkey0@gmail.com>
2023-01-23 22:23:14 -0600

    Read comment in file.

--- lib/util/motd.rb
+++ lib/util/motd.rb
@@ -1,4 +1,5 @@
-require 'fileutils'
+# Really basic helper function to display the message of the day. I might adapt
+# this into a generic function to display any text file in the future.
 
 def print_motd()
     motd = File.open('assets/motd.txt', 'r')
